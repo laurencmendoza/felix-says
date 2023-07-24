1965e805023da5b5371be7c2547ebc64069b645e
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-07-24 06:49:56 -0700

    Added handleClick function

--- main.js
+++ main.js
@@ -16,12 +16,14 @@ console.log('JS:loaded')
 const imgFelixMouthClosed = './imgs/Felix_mouth_closed.png'
 const imgFelixMouthOpen = './imgs/Felix_mouth_open.png'
 
+const options = ['red', 'green', 'yellow', 'blue']
 
 /*----- state variables -----*/ 
 
 let score = 0
 
-const scoreEl = document.querySelector('.score')
+let computerSequence = []
+let playerSequence = []
 
 
 
@@ -30,23 +32,37 @@ const redBtnEl = document.querySelector('#red-button')
 const greenBtnEl = document.querySelector('#green-button')
 const yellowBtnEl = document.querySelector('#yellow-button')
 const blueBtnEl = document.querySelector('#blue-button')
+const simonBtnEls = document.querySelectorAll('.simon > button')
+
+
+
+const startOverBtnEl = document.querySelector('#start-over-btn')
+
+const scoreEl = document.querySelector('.score')
 
 const felixImgEl = document.querySelector('#felix-img')
-const felixMouthClosed = 
-felixImgEl.setAttribute('src', imgFelixMouthOpen)
+
 /*----- event listeners -----*/ 
 
-redBtnEl.addEventListener('click', function() {
-    console.log('Red clicked')
-})
-greenBtnEl.addEventListener('click', function() {
-    console.log('Green clicked')
-})
-yellowBtnEl.addEventListener('click', function() {
-    console.log('Yellow clicked')
-})
-blueBtnEl.addEventListener('click', function() {
-    console.log('Blue clicked')
+// redBtnEl.addEventListener('click', function() {
+//     console.log('Red clicked')
+// })
+// greenBtnEl.addEventListener('click', function() {
+//     console.log('Green clicked')
+// })
+// yellowBtnEl.addEventListener('click', function() {
+//     console.log('Yellow clicked')
+// })
+// blueBtnEl.addEventListener('click', function() {
+//     console.log('Blue clicked')
+// })
+
+//making the above eventlisteners more DRY
+
+
+startOverBtnEl.addEventListener('click', function() {
+    console.log('Start over button clicked')
+    init()
 })
 
 /*----- functions -----*/ 
@@ -56,6 +72,35 @@ function init() {
     felixImgEl.setAttribute('src', imgFelixMouthClosed)
     // TO DO: Add the line of code below to render computer sequence function
     // felixImgEl.setAttribute('src', imgFelixMouthOpen)
+
+    simonBtnEls.forEach(function(btn) {
+        btn.addEventListener('click', handleClick)
+    })
+
+    addToComputerSequence()
+
 }
+
+function handleClick(evt) {
+    let btnColor = evt.target.classList.value
+    playerSequence.push(btnColor)
+
+    console.log(`Player's Sequence: ${playerSequence}`) 
+
+    // need a compare function 
+}
+
+console.log(Math.floor(Math.random() * 4)) // generates random number 0-3
+
+function addToComputerSequence() {
+    let randomIndex = Math.floor(Math.random() *4)
+    let computerChoice = options[randomIndex]
+    computerSequence.push(computerChoice)
+    console.log(computerSequence)
+    // pick a random color
+    // add random color to sequence
+}
+
+
 
 init()
